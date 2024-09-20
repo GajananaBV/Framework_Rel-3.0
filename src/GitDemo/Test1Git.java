0f621a711a815ae389c18d6cc9a1d09d1cb580cd
Gajanan <gajananbhange200@gmail.com>
2024-09-20 23:03:41 +0530

    updated with test3 and test4

--- src/GitDemo/Test1Git.java
+++ src/GitDemo/Test1Git.java
@@ -8,10 +8,20 @@ public class Test1Git
 		
 		System.out.println("Test1");
 	}
-	public void Test()
+	public void Test2()
 	{
 		
 		System.out.println("Test2");
+	}
+	public void Test3()
+	{
+		
+		System.out.println("Test3");
+	}
+	public void Test4()
+	{
+		
+		System.out.println("Test4");
 	}
 
 }
