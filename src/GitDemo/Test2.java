44d79f87ad1f80c26b4717dea8ac3c34aa525b89
Gajanan <gajananbhange200@gmail.com>
2024-09-20 23:20:54 +0530

    test updated by GitX

---
+++ src/GitDemo/Test2.java
@@ -0,0 +1,15 @@
+package GitDemo;
+
+public class Test2 
+{
+	public void Test1()
+	{
+		
+		System.out.println("Test1");
+	}
+	public void Test2()
+	{
+		
+		System.out.println("Test2");
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module US {
+	requires org.testng;
 }
