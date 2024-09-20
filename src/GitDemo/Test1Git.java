713ded2f7f441081a3cbd3fcc1daf33790d4cb64
Gajanan <gajananbhange200@gmail.com>
2024-09-20 22:54:52 +0530

    GitX commit US shift code

---
+++ src/GitDemo/Test1Git.java
@@ -0,0 +1,17 @@
+package GitDemo;
+
+public class Test1Git 
+{
+	
+	public void Test1()
+	{
+		
+		System.out.println("Test1");
+	}
+	public void Test()
+	{
+		
+		System.out.println("Test2");
+	}
+
+}
